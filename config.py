7234f046e0422378dbbbbeb0836b6c85ab5c773a
Strazil001 <strazil001@gmail.com>
2023-01-16 14:00:01 +0100

    init

---
+++ config.py
@@ -0,0 +1,9 @@
+import catppuccin
+
+
+# Load your autoconfig, use this if the rest of your config is empty!
+config.load_autoconfig()
+
+# Set the flavour you'd like to use
+# Valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
+catppuccin.setup(c, 'macchiato')
